c8cbb10bbbc8fa35aca2cfcdc947192c75a7c61d
Diamante710 <diamante710@icloud.com>
2023-03-26 19:00:25 -0600

    index model file added

--- models/index.js
+++ models/index.js
@@ -1,16 +1,36 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(
+  Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Product.belongToMany(
+  Tag, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'product_tag'
+});
 
-// Products belongToMany Tags (through ProductTag)
+Category.hasMany(
+  Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongToMany(
+  Product, {
+  through: {
+    model: ProductTag,
+    unique: false,
+  },
+  as: 'tag_product'
+});
 
 module.exports = {
   Product,
